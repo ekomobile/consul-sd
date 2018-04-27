d06fe6e8afea324af1b4797a3d5792f5e42801a2
Alexander Zhuravlev <a.zhuravlev@ekomobile.ru>
2018-04-27 12:58:52 +0300

    init

---
+++ default.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+	"net"
+
+	"github.com/hashicorp/consul/api"
+)
+
+type serviceDiscovery struct {
+	consul *api.Client
+}
+
+func NewServiceDiscovery() (ServiceDiscovery, error) {
+	c, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		return nil, err
+	}
+
+	return &serviceDiscovery{
+		consul: c,
+	}, nil
+}
+
+func (sd *serviceDiscovery) Get(id string) (Service, error) {
+	ss, _, err := sd.consul.Catalog().Service(id, "", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ss) == 0 {
+		return nil, NoServiceError
+	}
+
+	s := ss[0]
+
+	address := s.ServiceAddress
+	port := s.ServicePort
+
+	if address == "" {
+		address = s.Address
+	}
+
+	return &service{
+		address: address,
+		port:    port,
+	}, nil
+}
+
+type service struct {
+	address string
+	port    int
+}
+
+func (s *service) Addr() (net.Addr, error) {
+	return net.ResolveTCPAddr("ip", s.address)
+}
+
+func (s *service) MustAddr() net.Addr {
+	tcp, err := net.ResolveTCPAddr("ip", s.address)
+	if err != nil {
+		panic(err)
+	}
+
+	return tcp
+}
+
+func (s *service) Address() string {
+	return s.address
+}
+
+func (s *service) Port() int {
+	return s.port
+}
---
+++ sd.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"errors"
+	"net"
+)
+
+var NoServiceError = errors.New("no service")
+
+type ServiceDiscovery interface {
+	Get(id string) (Service, error)
+}
+
+type Service interface {
+	Address() string
+	Port() int
+	Addr() (net.Addr, error)
+	MustAddr() net.Addr
+}
